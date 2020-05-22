95bad2c79c813d0d15d3b26e24eeea537c2afabd
huanghaitao <A10696@tvt.net.cn>
2020-05-22 18:28:05 +0800

    添加树的数据结构

--- myBinarySortTree/src/com/ecjtu/hht/BSTNode.java
+++ myBinarySortTree/src/com/ecjtu/hht/BSTNode.java
@@ -8,12 +8,12 @@ import java.util.Comparator;
  * @author hht
  * @date 2020/5/11 11:08
  */
-public class BSTNode<T> {
+public class BSTNode {
 
     /**
      * 数据域
      */
-    private T value;
+    private long value;
 
     /**
      * 左孩子结点
@@ -27,66 +27,53 @@ public class BSTNode<T> {
 
 
     /**
-     * 比较器
+     * 有参构造 添加一个根结点
      */
-    private final Comparator<? super T> comparator;
-
+    public BSTNode(long value) {
+        this.value = value;
+    }
 
     /**
      * 有参构造 添加一个根结点
      */
-    public BSTNode(T value) {
-        this.value = value;
-        comparator = null;
-    }
-
-    /**
-     * 比较器添加
-     */
-    public BSTNode(Comparator<? super T> comparator) {
-        this.comparator = comparator;
-    }
-
-    /**
-     * 无参构造
-     */
     public BSTNode() {
-        this.comparator = null;
     }
 
     /**
      * 添加结点
-     *
-     * @return
      */
-    public void insertNode(T newValue) {
-
-        //如果插入当前结点值为null  将新的值作为根结点
-        if (this.value == null) {
-            this.value = newValue;
-            return;
-        }
-        BSTNode newNode = new BSTNode(this.comparator);
-        //否则将当前值与新的值比较  > 查找  右子树  < 查找左子树
-        if (compare(this.value, newValue) > 0) {
-            this.rightChildNode = newNode;
-            this.rightChildNode.insertNode(newValue);
+    public static void insertNode(BSTNode bstNode, long newValue) {
+        if (bstNode == null) {
+            System.out.println(bstNode);
+            bstNode = new BSTNode(newValue);
+            bstNode.leftChildNode = bstNode.rightChildNode = null;
+        } else if (bstNode.value > newValue) {
+            insertNode(bstNode.rightChildNode, newValue);
         } else {
-            this.leftChildNode = newNode;
-            this.leftChildNode.insertNode(newValue);
+            insertNode(bstNode.leftChildNode, newValue);
         }
     }
 
+
     /**
-     * 比较值的大小
-     *
-     * @param k1
-     * @param k2
-     * @return
+     * 中序遍历
      */
-    final int compare(Object k1, Object k2) {
-        return comparator == null ? ((Comparable<T>) k1).compareTo((T) k2)
-                : comparator.compare((T) k1, (T) k2);
+    public void inTraverseTree(BSTNode rootNode) {
+        if (rootNode != null) {
+            inTraverseTree(rootNode.leftChildNode);
+            System.out.println(rootNode.value);
+            inTraverseTree(rightChildNode.rightChildNode);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        BSTNode bstNode = new BSTNode(4);
+        insertNode(bstNode, 2);
+        insertNode(bstNode, 3);
+        //insertNode(bstNode, 6);
+        // bstNode.insertNode(4L).insertNode(2L).insertNode(3L).insertNode(6L);
+        //bstNode.inTraverseTree();
     }
 
 }
